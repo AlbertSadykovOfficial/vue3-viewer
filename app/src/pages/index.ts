3c7892bf4292c74d0b9b90e284cdefc227bbc229
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-24 20:31:27 +0300

    three finally page added to routing

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -1,7 +1,8 @@
 import Routing from "./index.vue";
 
 export const routes = [
-    { path: "/", component: () => import("./home/"), name: "Home", }
+    { path: "/", component: () => import("./home/"), name: "Home", },
+    { path: "/three-finally", component: () => import("./three_finally/"), name: "ThreeFinally", }
 ];
 
 export { Routing };
