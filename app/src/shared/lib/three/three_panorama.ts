7d3f27de83b8576f4b818e0c65decad5862d894a
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-24 20:29:40 +0300

    three shared lib

---
+++ app/src/shared/lib/three/three_panorama.ts
@@ -0,0 +1,15 @@
+export {
+  TextureLoader,
+  Vector3,
+  Texture,
+  Mesh,
+  CircleGeometry,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  Raycaster,
+  Vector2,
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  SphereGeometry
+} from 'three'
