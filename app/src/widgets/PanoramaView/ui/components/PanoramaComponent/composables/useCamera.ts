6d108bad910524f0e02576d55585b2406bfa4a6f
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-25 11:39:03 +0300

    use camera camera update set to default method update

--- app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useCamera.ts
+++ app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useCamera.ts
@@ -1,15 +1,36 @@
 import { PerspectiveCamera } from '@/shared/lib/three';
-
-export default function useCamera(fov, ratio, a, b) {
-  const camera = new PerspectiveCamera(fov, ratio, a, b);
+import { PerspectiveCamera } from 'three'
+export default function useCamera(fov, ratio, near, far) {
+  const camera = new PerspectiveCamera(fov, ratio, near, far);
   camera.position.set(0, 0, 2);
 
   /**
-   * Выставить значение fov в исходные параметры
+   * Обновить
+   */
+  const cameraUpdate = (camera) => {
+    camera.updateProjectionMatrix();
+  }
+
+  /**
+   * Выставить значение камеры в исходные параметры
+   */
+  const cameraSetDefault = (camera) => {
+    camera.fov = fov
+    camera.position.set(0, 0, 2);
+    /*
+    * Cбрасываем кватернион к его начальному значению (единичному кватерниону, что соответствует нулевому вращению).
+    * (x=0, y=0, z=0, w=1)
+    * [При анимации движения к кнопке кватернион изменяется, поэтому сбрасываем его, иначе будет перекос]
+    */
+    camera.quaternion.identity()
+  }
+
+  /**
+   * Установить начальные значения и обновить
    */
   const cameraToDefault = (camera) => {
-    camera.fov = fov
-    camera.updateProjectionMatrix();
+    cameraSetDefault(camera)
+    cameraUpdate(camera)
   }
 
   return { camera, cameraToDefault };
