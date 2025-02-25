088bfccaaae456c9f25cf33ff158b14eb552a128
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-25 16:01:27 +0300

    useControls composable to TS

--- app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useControls.ts
+++ app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useControls.ts
@@ -1,6 +1,7 @@
 import { OrbitControls } from '@/shared/lib/three/examples'
+import type { PerspectiveCamera, WebGLRenderer  } from '@/shared/lib/three';
 
-export default function useControls(camera, renderer, callback: () => {}) {
+export default function useControls(camera: PerspectiveCamera, renderer: WebGLRenderer, callback: () => void = () => {}) {
   const controls = new OrbitControls(camera, renderer.domElement);
 
   const startControlsChangeListener = () => {
