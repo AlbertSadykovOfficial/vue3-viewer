b2a4deba3f1f45a239a5c8e3e80d9abab3d34ba1
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-25 16:35:02 +0300

    useNavigationButtons composable to TS (Promise)

--- app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useNavigationButtons.ts
+++ app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useNavigationButtons.ts
@@ -1,15 +1,30 @@
-import { Raycaster, Vector2 } from '@/shared/lib/three';
 import useCreateNavigationButton from './useCreateNavigationButton'
 
-export default function useNavigationButtons(scene, camera, renderer, textureLoader, callback: () => {}) {
-  /** @type {{ three_mesh: THREE.Mesh, nextPanoramaKey: string }} Кнопки навигации */
-  const navigationButtons = {}
+import { Raycaster, Vector2 } from '@/shared/lib/three';
+
+import type { Scene, PerspectiveCamera, Mesh, TextureLoader, WebGLRenderer } from '@/shared/lib/three'
+import type { TPanoramaButton } from "@/widgets/PanoramaView/model/types";
+type TNavigationButton = TPanoramaButton & {
+  three_mesh: Mesh
+}
+type TNavigationButtons = { [name: string]: TNavigationButton }
+
+export default function useNavigationButtons(
+  scene: Scene,
+  camera: PerspectiveCamera,
+  renderer: WebGLRenderer,
+  textureLoader: TextureLoader,
+  callback: (button?: TNavigationButton) => void = () => {}
+) {
+
+  /** @type {TNavigationButtons} Кнопки навигации */
+  const navigationButtons: TNavigationButtons = {}
   const { createNavigationButton } = useCreateNavigationButton(textureLoader)
 
   /**
    * Создать кнопки навигации панорамы и добавить их на рендер
    */
-  const createNavigationButtons = (buttons, camera) => {
+  const createNavigationButtons = (buttons: TNavigationButtons, camera: PerspectiveCamera) => {
     for (const i in buttons) {
       const button = createNavigationButton(camera, buttons[i].position)
       navigationButtons[button.uuid] = {
@@ -24,7 +39,7 @@ export default function useNavigationButtons(scene, camera, renderer, textureLoa
   /**
    * Удалить все кнопки навигации с рендера и объекта хранения
    */
-  const deleteAllButtons = (buttons = navigationButtons) => {
+  const deleteAllButtons = (buttons: TNavigationButtons = navigationButtons) => {
     for (const key in buttons) {
       scene.remove(buttons[key].three_mesh)
       delete buttons[key]
@@ -38,7 +53,7 @@ export default function useNavigationButtons(scene, camera, renderer, textureLoa
      * то перейти на панораму, привязанную к этой кнопке
      * @param {THREE.Raycaster} raycaster
      */
-    const processButtonIntersection = (raycaster) => {
+    const processButtonIntersection = (raycaster: Raycaster) => {
       for (const key in navigationButtons) {
         const intersects = raycaster.intersectObject(navigationButtons[key].three_mesh)
         if (intersects.length > 0) {
@@ -52,7 +67,7 @@ export default function useNavigationButtons(scene, camera, renderer, textureLoa
      * Обработать событие нажатия на рендер
      * @param {MouseEvent} event - Событие нажатия
      */
-    const onRenderClick = (event) => {
+    const onRenderClick = (event: MouseEvent) => {
       const raycaster = new Raycaster();
       const mouse = new Vector2();
       mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
