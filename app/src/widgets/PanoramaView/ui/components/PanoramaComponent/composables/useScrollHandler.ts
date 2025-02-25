e6008fe5f54cbc3c1fe66ca141d9f24e138229b0
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-25 16:39:43 +0300

    useScrollHandler composable to TS

--- app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useScrollHandler.ts
+++ app/src/widgets/PanoramaView/ui/components/PanoramaComponent/composables/useScrollHandler.ts
@@ -1,8 +1,8 @@
-import { Vector3 } from '@/shared/lib/three';
+import type { PerspectiveCamera, WebGLRenderer } from '@/shared/lib/three'
 
-export default function useScrollHandler(camera, renderer, callback= ()=>{}) {
+export default function useScrollHandler(camera: PerspectiveCamera, renderer: WebGLRenderer, callback: () => void = ()=>{}) {
   const startScrollListener = () => {
-    renderer.domElement.addEventListener('wheel', (event) => {
+    renderer.domElement.addEventListener('wheel', (event: WheelEvent) => {
       camera.fov += event.deltaY * 0.05;
       camera.fov = Math.max(20, Math.min(90, camera.fov));
       camera.updateProjectionMatrix();
