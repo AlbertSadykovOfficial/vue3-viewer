e9c85fbadf3075e6345b4c055e386fce51a978d1
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-25 18:32:16 +0300

    Panorama Component megrated to setup script

--- app/src/widgets/PanoramaView/ui/components/PanoramaComponent/index.vue
+++ app/src/widgets/PanoramaView/ui/components/PanoramaComponent/index.vue
@@ -2,8 +2,8 @@
   <div ref="container" class="scene-container"></div>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+<script setup lang="ts">
+import { defineOptions, defineProps, defineEmits, onMounted, ref } from 'vue';
 import { TextureLoader, Vector3 } from '@/shared/lib/three';
 
 import useScene from "./composables/useScene"
@@ -17,160 +17,169 @@ import useAnimate from "./composables/useAnimate";
 import useNavigationButtons from './composables/useNavigationButtons'
 import useScrollHandler from "./composables/useScrollHandler"
 
-import type { PropType } from 'vue'
+// ======================= //
+//    Types Declaration    //
+// ======================= //
 import type { TPanoramaDict } from "../../../model/types";
 
-export default defineComponent({
-  props: {
-    panoramas: {
-      type: Object as PropType<TPanoramaDict>,
-      required: true,
-    }
-  },
 
-  setup(props, { emit }) {
-    // ======================= //
-    //          Props          //
-    // ======================= //
-    /** @type {Object} Объект с панорамами */
-    const { panoramas } = props
+// ======================= //
+//           Page          //
+// ======================= //
+defineOptions({
+  name: 'PanoramaComponent',
+})
 
-    // ======================= //
-    //        Constants        //
-    // ======================= //
+const emit = defineEmits<{
+  'load-started': [value: number]
+  'tile-loaded': [],
+  'load-ended': []
+}>()
 
-    /** @type {{value: HTMLElement}} Контейнер для canvas */
-    const container = ref<HTMLDivElement>();
+// ======================= //
+//          Props          //
+// ======================= //
+const { panoramas } = defineProps<{
+  /** @type {TPanoramaDict} Объект с панорамами */
+  panoramas: TPanoramaDict
+}>()
 
-    // ======================= //
-    //          SETUP          //
-    // ======================= //
+
+// ======================= //
+//      Template Refs      //
+// ======================= //
+/** @type {{value: HTMLElement}} Контейнер для canvas */
+const container = ref<HTMLDivElement>();
+
+
+// ======================= //
+//        Functions        //
+// ======================= //
+
+const setup = () => {
+  // ======================= //
+  //        Variables        //
+  // ======================= //
+  /** @type {string} currentPanorama - Ключ Выбранной панормы */
+  let currentPanorama = Object.keys(panoramas)[0];
+
+  // ======================= //
+  //        Constants        //
+  // ======================= //
+  const DEFAULT_FOV = 75
+  const [width, height] = [window.innerWidth, window.innerHeight]
+  const ratio = width / height
+
+  /** @type {THREE.TextureLoader} Загрузчик текстур */
+  const textureLoader = new TextureLoader();
+
+  const { scene } = useScene();
+  const { camera, cameraToDefault } = useCamera(DEFAULT_FOV, ratio, 0.1, 1000);
+  const { renderer, pushRenderer } = useRenderer(width, height, { antialias: true });
+  const { sphere } = useSphere()
+
+  const { startControlsChangeListener } = useControls(camera, renderer,() => {
+    loadAndRenderTiles(panoramas[currentPanorama], camera, sphere)
+  })
+
+  const { bindAndLoad, loadAndRenderTiles } = useLoadAndRenderTiles(textureLoader, {
+    onStartLoading:  (total) => emit('load-started', total),
+    onTileLoaded: () => emit('tile-loaded'),
+    onFinishLoading: () => emit('load-ended')
+  })
+
+  const { animateZoomToPoint } = useAnimate(800, 30)
+
+  const { startButtonClickListener, createNavigationButtons, deleteAllButtons } = useNavigationButtons(scene, camera, renderer, textureLoader,(button) => {
     /**
-     * Метод инициализации панорамы
+     * Чтоб не ждать завершения анимации
+     * Подгружаем [в кэш]! тайлы панорамы на которую переходим
+     * (Но не рендерим)
      */
-    const setup = () => {
-      // ======================= //
-      //        Variables        //
-      // ======================= //
-      /** @type {string} currentPanorama - Ключ Выбранной панормы */
-      let currentPanorama = Object.keys(panoramas)[0];
+    bindAndLoad(panoramas[button.nextPanoramaKey], camera)
 
-      // ======================= //
-      //        Constants        //
-      // ======================= //
-      const DEFAULT_FOV = 75
-      const [width, height] = [window.innerWidth, window.innerHeight]
-      const ratio = width / height
-
-      /** @type {THREE.TextureLoader} Загрузчик текстур */
-      const textureLoader = new TextureLoader();
-
-      const { scene } = useScene();
-      const { camera, cameraToDefault } = useCamera(DEFAULT_FOV, ratio, 0.1, 1000);
-      const { renderer, pushRenderer } = useRenderer(width, height, { antialias: true });
-      const { sphere } = useSphere()
-
-      const { startControlsChangeListener } = useControls(camera, renderer,() => {
-        loadAndRenderTiles(panoramas[currentPanorama], camera, sphere)
-      })
-
-      const { bindAndLoad, loadAndRenderTiles } = useLoadAndRenderTiles(textureLoader, {
-        onStartLoading:  (total) => emit('load-started', total),
-        onTileLoaded: () => emit('tile-loaded'),
-        onFinishLoading: () => emit('load-ended')
-      })
-
-      const { animateZoomToPoint } = useAnimate(800, 30)
-
-      const { startButtonClickListener, createNavigationButtons, deleteAllButtons } = useNavigationButtons(scene, camera, renderer, textureLoader,(button) => {
-        /**
-         * Чтоб не ждать завершения анимации
-         * Подгружаем [в кэш]! тайлы панорамы на которую переходим
-         * (Но не рендерим)
-         */
-        bindAndLoad(panoramas[button.nextPanoramaKey], camera)
-
-        /**
-         * Производим анимацию зума к точке
-         * После чего меняем текущую панораму
-         * Затем ререндерим
-         */
-        const { x, y, z } = button.position
-        const nextPanoramaKey = button.nextPanoramaKey
-        deleteAllButtons();
-        animateZoomToPoint(camera, new Vector3(x, y, z), () => {
-          renderer.domElement.style.transition = "opacity 0.3s";
-          renderer.domElement.style.opacity = "0";
-
-          /**
-           * Откладываем смену панорамы на время анимации (transition opacity),
-           * чтобы не было мерцания
-           * Затем ререндерим
-           */
-          setTimeout(async () => {
-            currentPanorama = nextPanoramaKey;
-            await refreshScene();
-            // Показываем панораму после обновления рендера
-            renderer.domElement.style.opacity = "1";
-          }, 300);
-        });
-      })
-
-      const { startScrollListener } = useScrollHandler(camera, renderer, () => {
-        loadAndRenderTiles(panoramas[currentPanorama], camera, sphere)
-      })
+    /**
+     * Производим анимацию зума к точке
+     * После чего меняем текущую панораму
+     * Затем ререндерим
+     */
+    const { x, y, z } = button.position
+    const nextPanoramaKey = button.nextPanoramaKey
+    deleteAllButtons();
+    animateZoomToPoint(camera, new Vector3(x, y, z), () => {
+      renderer.domElement.style.transition = "opacity 0.3s";
+      renderer.domElement.style.opacity = "0";
 
       /**
-       * Обновление рендера
-       * Удаляем все кнопки панорамы
-       * Устанавливаем дефолтные значения камеры
-       * Подгружаем и рендерим тайлы в области видимости
-       * Создаем кнопки для этой панорамы
+       * Откладываем смену панорамы на время анимации (transition opacity),
+       * чтобы не было мерцания
+       * Затем ререндерим
        */
-      const refreshScene = async () => {
-        deleteAllButtons();
-        cameraToDefault(camera);
-        await loadAndRenderTiles(panoramas[currentPanorama], camera, sphere);
-        createNavigationButtons(panoramas[currentPanorama].buttons, camera);
-      }
-
-      const animate = () => {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-      };
-
-      if (container.value instanceof HTMLDivElement) {
-        // Поместить рендер в контейнер шаблона
-        pushRenderer(container.value, renderer)
-      }
-
-      // Добавить на сцену сферическое представление
-      scene.add(sphere);
-
-      // Начинаем отслеживать действия с элементом управления
-      startControlsChangeListener()
-
-      // Начинаем отслеживать нажатие на кнопку
-      startButtonClickListener()
-
-      // Начинаем отслеживать скролл
-      startScrollListener()
-
-
-      // Обновляем состояние сцены
-      refreshScene();
-
-      // Начинаем отрисовку
-      animate();
-    }
-
-    onMounted(() => {
-      if (container.value instanceof HTMLDivElement) {
-        setup()
-      }
+      setTimeout(async () => {
+        currentPanorama = nextPanoramaKey;
+        await refreshScene();
+        // Показываем панораму после обновления рендера
+        renderer.domElement.style.opacity = "1";
+      }, 300);
     });
+  })
 
-    return { container };
+  const { startScrollListener } = useScrollHandler(camera, renderer, () => {
+    loadAndRenderTiles(panoramas[currentPanorama], camera, sphere)
+  })
+
+  /**
+   * Обновление рендера
+   * Удаляем все кнопки панорамы
+   * Устанавливаем дефолтные значения камеры
+   * Подгружаем и рендерим тайлы в области видимости
+   * Создаем кнопки для этой панорамы
+   */
+  const refreshScene = async () => {
+    deleteAllButtons();
+    cameraToDefault(camera);
+    await loadAndRenderTiles(panoramas[currentPanorama], camera, sphere);
+    createNavigationButtons(panoramas[currentPanorama].buttons, camera);
+  }
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  };
+
+  if (container.value instanceof HTMLDivElement) {
+    // Поместить рендер в контейнер шаблона
+    pushRenderer(container.value, renderer)
+  }
+
+  // Добавить на сцену сферическое представление
+  scene.add(sphere);
+
+  // Начинаем отслеживать действия с элементом управления
+  startControlsChangeListener()
+
+  // Начинаем отслеживать нажатие на кнопку
+  startButtonClickListener()
+
+  // Начинаем отслеживать скролл
+  startScrollListener()
+
+
+  // Обновляем состояние сцены
+  refreshScene();
+
+  // Начинаем отрисовку
+  animate();
+}
+
+
+// ======================= //
+//         Events          //
+// ======================= //
+
+onMounted(() => {
+  if (container.value instanceof HTMLDivElement) {
+    setup()
   }
 });
 </script>
